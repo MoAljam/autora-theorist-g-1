fd3a0f0b36cf7acf41e1ddcd4748ae2c01180fdc
rmathew-uni <rmathew@uni-osnabrueck.de>
2024-07-31 16:36:27 +0200

    added evaluator for equation. does not work for np arrays.

--- src/autora/utils.py
+++ src/autora/utils.py
@@ -1,3 +1,6 @@
+from typing import Union
+import numpy as np
+import math
 
 def print_equation(equation, operator_space) -> str:
     equation_str = ""
@@ -15,4 +18,32 @@ def print_equation(equation, operator_space) -> str:
             i -= 1
     return equation_str
 
+def equation_evaluator(equation, operator_space, variable_space, data) -> int | float|np.array:
+#Union[int, float, nd.ndarray]:
+    stack = []
+    variables = []
 
+    for i in equation[::-1]:
+        if not i in operator_space:
+            stack.append(data[i])
+        else:
+            for j in range(0, operator_space[i]):
+                variables.append(stack.pop)
+                j+=1
+            
+            if i == '+':
+                stack.append(variables[0] + variables[1])
+            elif i == '-':
+                stack.append(variables[0] - variables[1])
+            elif i == '*':
+                stack.append(variables[0] * variables[1])
+            elif i == '/':
+                stack.append(variables[0] / variables[1])
+            elif i == 'exp':
+                stack.append(math.exp(variables[0]))
+            elif i == 'ln':
+                stack.append(l=math.log(variables[0]))
+            elif i == 'pow':
+                stack.append(variables[0] ^ variables[1])
+        
+    return stack.pop()
