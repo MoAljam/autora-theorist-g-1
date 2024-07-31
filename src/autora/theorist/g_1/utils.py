3ec8fb995f7f6d807cbc8d70e745e24b9586b8ca
MoAljam <maljammal@uni-osnabrueck.de>
2024-07-31 18:27:51 +0200

    working MCMC exmaple model

--- src/autora/theorist/g_1/utils.py
+++ src/autora/theorist/g_1/utils.py
@@ -10,9 +10,13 @@ def print_equation(equation, operator_space) -> str:
         if not equation[i] in operator_space:
             variable_stack.append(equation[i])
         else:
-            equation_str ="(" + variable_stack.pop() + equation[i] + variable_stack.pop() + ")"
+            print("## variable_stack: ", variable_stack)
+            if operator_space[equation[i]] == 1:
+                equation_str ="(" + equation[i] + "("+variable_stack.pop()+")" + ")"
+            else:
+                equation_str ="(" + variable_stack.pop() + equation[i] + variable_stack.pop() + ")"
             variable_stack.append(equation_str)
-            i -= 1
+        i -=1
     return equation_str
 
 
