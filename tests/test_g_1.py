20c2b0665b5dbc51833205b60a6d7419d4bb2a2f
MoAljam <maljammal@uni-osnabrueck.de>
2024-09-30 23:57:01 +0200

    automated documentation

--- tests/test_g_1.py
+++ tests/test_g_1.py
@@ -1,8 +1,6 @@
 from autora.theorist.g_1 import CustomMCMC
 
+
 def test():
     theorist = CustomMCMC()
     assert theorist is not None
-
-
-# Note: We encourage you to adjust this test and write more tests.
